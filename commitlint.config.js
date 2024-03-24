60f765fa488a1980dcb4b07dabcd06a6cebd85ab
BaMau <bamau97@gmail.com>
2024-03-24 10:22:36 +0700

    feat: add rule committing (dev)

---
+++ commitlint.config.js
@@ -0,0 +1,45 @@
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+// build: Changes that affect the build system or external dependencies
+// chore: used for miscellaneous changes that don't affect the main codebase(configuring development tools, setting up project-specific settings)
+// ci: Changes to our CI configuration files and scripts
+// docs: Documentation only changes
+// feat: A new feature
+// fix: A bug fix
+// perf: A code change that improves performance
+// refactor: A code change that neither fixes a bug nor adds a feature
+// style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+// test: Adding missing tests or correcting existing tests
+// translation: Changes related to translations or language localization. This includes adding or updating translations in the codebase.
+// security: Changes that address security vulnerabilities, implement security measures, or enhance the overall security of the codebase.
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'body-case': [2, 'always', 'sentence-case'],
+    'body-max-line-length': [1, 'always', 72],
+    'header-max-length': [1, 'always', 52],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'change',
+        'chore',
+        'ci',
+        'deprecate',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'remove',
+        'revert',
+        'security',
+        'style',
+        'test',
+      ],
+    ],
+  },
+};
