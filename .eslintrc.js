14a5fa58d3aa91af8c6da98a720101cc0df329a5
BaMau <bamau97@gmail.com>
2024-03-31 11:03:56 +0700

    chore(config): update config eslintrc, prettier and husky (dev)

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,60 +13,34 @@ module.exports = {
     node: true,
   },
   root: true,
-  extends: [
-    'next',
-    'eslint:recommended',
-    'prettier',
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks'],
+  extends: ['next', 'eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   rules: {
     // JavaScript rules
-    'prefer-const': 'warn',
     'no-var': 'warn',
+    'no-plusplus': 'off',
     'no-unused-vars': 'warn',
-    'object-shorthand': 'warn',
+    'no-param-reassign': 'off',
+    'no-case-declarations': 'off',
     'quote-props': ['warn', 'as-needed'],
+    'import/prefer-default-export': 'off',
     // TypeScript rules
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/array-type': [
-      'warn',
-      {
-        default: 'array',
-      },
-    ],
-    '@typescript-eslint/consistent-type-assertions': [
-      'warn',
-      {
-        assertionStyle: 'as',
-        objectLiteralTypeAssertions: 'never',
-      },
-    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
     // React rules
-    'react/jsx-fragments': ['warn', 'syntax'], // Shorthand syntax for React fragments
     'react/jsx-filename-extension': [
       'warn',
       {
         extensions: ['ts', 'tsx'],
       },
     ],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'prettier/prettier': 'error',
-    'import/prefer-default-export': 'off',
-    'no-param-reassign': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    'no-plusplus': 'off',
-    'no-case-declarations': 'off',
   },
   settings: {
     react: {
