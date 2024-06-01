a73958f6edd8ce5e94c127b44dd3f2476fd5b19c
BaMau <bamau97@gmail.com>
2024-06-01 16:25:50 +0700

    test: test commitlint (dev)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+/*
+ * @Author: BaMau
+ * @Date: 2024-06-01 15:28:31
+ * @Last Modified by: BaMau
+ * @Last Modified time: 2024-06-01 16:23:01
+ */
+
 import Image from 'next/image'
+
+import TestComponent from '../components/test/test-component'
 
 import './globals.css'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center  justify-between p-24'>
+      <TestComponent />
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200  lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30'>
           Get started by editing&nbsp;
